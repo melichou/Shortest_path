ca12848454d3cbf5cf16443f522694d4ff076556
Melichou <silvafernandesmelissa@gmail.com>
2022-05-05 14:19:31 +0200

    implemented attributes to classes map and point

--- src/Classes/Map.php
+++ src/Classes/Map.php
@@ -1,3 +1,19 @@
 <?php
+class Map{
+    //Attributes 
+    private array $map;
 
+    //Constructor
+    public function __construct(array $map){
+        $this->map = $map;
+    }
+
+    //Getter
+    public function getMap() : Map{
+        return $this;
+    }
+    public function getMapArray() : array{
+        return $this->map;
+    }
+}
 ?>
