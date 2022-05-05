2efbc9a7e0e279ee68a0e822c46076e05da2d0f0
Melichou <silvafernandesmelissa@gmail.com>
2022-05-05 15:11:50 +0200

    implemented path class and added getter to point class

--- src/Classes/Path.php
+++ src/Classes/Path.php
@@ -1,5 +1,15 @@
 <?php 
+require_once "Classes/Point.php";
+class Path{
+    //Attributes
+    private array $path;
 
+    //Functions
+    public function addPoint(Point $point){
+        $pos = $point->getPosition();
+        $this->path += $pos;
+    }
+}
 
 
 ?>
