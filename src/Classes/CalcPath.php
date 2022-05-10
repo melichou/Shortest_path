32b93f8dc715fa0b31672884b528ba7f38664c2d
melichou <71888605+melichou@users.noreply.github.com>
2022-05-10 11:54:37 +0200

    pushed functions to test it because I got an issue (#10)
    * implemented path class and added getter to point class
    
    * added path class and getter to point class
    
    * added CalcPath class and some functions
    
    * worked a little on Display and path class
    
    * edited addpoint function in path class
    
    * tried to implement function but got error, push to test on other machine
    
    * push to test code on other machine to see if we get the same error

--- src/Classes/CalcPath.php
+++ src/Classes/CalcPath.php
@@ -1,28 +1,144 @@
 <?php
-require_once "Classes/Path";
+require_once "Path.php";
 class CalcPath{
     //Attributes 
-    private Path $path;
+    private Map $map;
+    private Path $currentpath;
+    private Path $shortestpath;
+    private Point $startpoint;
+    private Point $endpoint;
 
     //Constructor
-    public function __construct(){
-
+    public function __construct(Map $currentmap, Point $startpoint, Point $endpoint){
+        $this->map = $currentmap;
+        $this->startpoint = $startpoint;
+        $this->endpoint = $endpoint;
+        $this->currentpath = new Path($startpoint);
     }
-    //Functions 
-    public function shortestPath(Path $shortest, Path $current) : Path{
-        //attributes 
-        $currentpath = $current->getPath;
-        $shortestpath =$shortest->getPath;
 
-        if ($currentpath < $shortestpath){
-            return $current;
+    //Getters
+    public function getCurrentPath() : Path{
+        return $this->currentpath;
+    }
+
+    //Functions 
+    public function addPoints(Point $thispoint, Path $currentpath){
+        //Getting position of current point
+        $currentpos = $thispoint->getPosition();
+        $endpos = $this->endpoint->getPosition();
+
+        //Getting map in array
+        $maparray = $this->map->getMapArray();
+
+        //Getting path in array
+        $path = $currentpath->getPath();
+
+        //Setting available points
+        $points = [
+            [$currentpos[0] - 1, $currentpos[1]],
+            [$currentpos[0] + 1, $currentpos[1]],
+            [$currentpos[0], $currentpos[1] - 1],
+            [$currentpos[0], $currentpos[1] + 1],
+        ];
+        //var_dump($points);
+
+        //Treatment 
+        foreach ($points as $point){
+            if ($point[0] < 0 || $point[1] < 0 || $point[0] >= count($maparray) || $point[1] >= count($maparray[0])){
+                continue;
+            }
+            else {
+                if (($maparray[$point[0]][$point[1]]) == 0){
+                    continue;
+                }
+                else {
+                    if(in_array($point,$path)){
+                        continue;
+                    }
+                    else {
+                        $valid = new Point($point[0], $point[1]);
+                        unset($points);
+                        var_dump($valid);
+                        $this->addPoints($valid, $currentpath);
+
+                        //checking if valable point is endpos
+                        if ($valid->getPosition() == $endpos){
+                            $this->currentpath = $currentpath;
+                            return $currentpath;
+                        }
+                        else {
+                            var_dump($currentpath);
+                            $this->addPoints($valid,$currentpath);
+                        }
+                    }
+                } 
+            }
         }
-        elseif ($currentpath > $shortestpath){
-            return $shortest;
+    }
+    
+
+    public function shortestPath(Path $currentpath){
+        //attributes 
+        $endpos = $this->endpoint->getPosition();
+        if (!empty($this->shortestPath) && in_array($endpos,$this->shortestPath->getPath())){
+            $shortest = $this->shortestpath->getPath();
+            if ($currentpath < $shortest){
+                return $this->currentpath;
+            }
+            elseif ($currentpath > $shortest){
+                return $this->shortestpath;
+            }
+            else {
+                throw new Exception("Les deux chemins sont équidistances");
+            }
         }
         else {
-            throw new Exception("Les deux chemins sont équidistances");
+            $this->shortestPath = $currentpath;
+            $newpath = new Path($this->startpoint);
+            $this->addPoints($this->startpoint,$newpath);
+            $this->shortestPath($this->currentpath);
         }
+        
+    }
+    public function testGetShortestPath(Point $point, Point $end, Path $path){
+        global $shortest;
+
+        if (!empty($shortest) && count($path->getPath()) >= count($shortest->getPath())){
+            return $shortest;
+        }
+
+        if($point->getAxeX() == $end->getAxeX() && $point->getAxeY() == $end->getAxeY()){
+            $shortest = $path;
+            return $path;
+        }
+
+        $currentpos = $point->getPosition();
+        $map = $this->map->getMapArray();
+
+        $points = [
+            [$currentpos[0] - 1, $currentpos[1]],
+            [$currentpos[0] + 1, $currentpos[1]],
+            [$currentpos[0], $currentpos[1] - 1],
+            [$currentpos[0], $currentpos[1] + 1],
+        ];
+
+        foreach ($points as $point) {
+            if ($point[0] < 0 || $point[1] < 0 || $point[0] >= count($map) || $point[1] >= count($map[0])) {
+                continue;
+            }
+            if (0 == $map[$point[0]][$point[1]]) {
+                continue;
+            }
+            if (in_array($point, $path->getPath())) {
+                continue;
+            }
+            $valid = new Point($point[0],$point[1]);
+
+            $this->testGetShortestPath($valid, $end, $path);
+        }
+
+
+    
     }
 }
 ?>
--- src/index.php
+++ src/index.php
@@ -1,5 +1,28 @@
 <?php
+require_once "Classes/CalcPath.php";
+require_once "Classes/Path.php";
+require_once "Classes/Map.php";
+require_once "Classes/Point.php";
+
+
 echo "test docker shortest path";
+
+$map = new Map([
+    [1,1,1,1,1],
+    [1,1,0,0,1],
+    [0,1,1,0,1],
+    [0,1,1,1,1],
+    [1,1,0,1,1],
+    [1,1,1,1,0],
+]);
+$start = new Point(2,4);
+$end = new Point(1,0);
+
+$calc = new CalcPath($map,$start,$end);
+$path = new Path($start);
+
+$short = $calc->testGetShortestPath($start, $end, $path);
+
 
 
 ?>
