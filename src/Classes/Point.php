a18a800ff4a17cf602de26ee18568b00dd207e0d
Melichou <silvafernandesmelissa@gmail.com>
2022-05-10 11:39:46 +0200

    push to test code on other machine to see if we get the same error

--- src/Classes/Point.php
+++ src/Classes/Point.php
@@ -1,14 +1,10 @@
 <?php
 class Point{
     //Attributes
-    private int $axeX;
-    private int $axeY;
     private array $position;
 
     //Constructor
     public function __construct(int $axeX, int $axeY){
-        $this->axeX = $axeX;
-        $this->axeY = $axeY;
         $this->position[0] = $axeX;
         $this->position[1] = $axeY;
     }
@@ -17,10 +13,10 @@ class Point{
         return $this->position; 
     }
     public function getAxeX() : int{
-        return $this->axeX;
+        return $this->position[0];
     }
     public function getAxeY() : int{
-        return $this->axeY;
+        return $this->position[1];
     }
 
 }
